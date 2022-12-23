e3348934f934944112899af94f5c6935e7e93431
Andela Stevanovic <andjela.s166@gmail.com>
2022-12-24 00:30:51 +0500

    Added authorization to edit a bottle

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -25,15 +25,23 @@ class BottlesController < ApplicationController
    #PATCH/PUT/bottles/1
   def update
     @bottle = Bottle.find(params[:id])
-    @bottle.update(title: params[:title], wine_type: params[:wineType], grape_variety: params[:grapeVariety], vintage: params[:vintage]) if @bottle.present?
+    if @bottle.user_id == @current_user.id
+      @bottle.update(title: params[:title], wine_type: params[:wineType], grape_variety: params[:grapeVariety], vintage: params[:vintage]) if @bottle.present?
+      render json: {message: 'Updated successfully', error: false}, status: :ok
+    else
+      render json: {error: true}, status: :unauthorized
+    end
   end
 
   
   def destroy
-    @bottle = @current_user.bottles.find(params[:id])
-    #run errors
-    @bottle.destroy
-    render json: { message: 'Deleted Successfully'}, status: :ok
+    @bottle = Bottle.find(params[:id])
+    if @bottle.user_id == @current_user.id
+      @bottle.destroy
+      render json: {message: 'Deleted Successfully', error: false}, status: :ok
+    else
+      render json: {error: true}, status: :unauthorized
+    end
   end
 end
 
